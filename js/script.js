d752ec087a5ac47f8eaebe6d9d5836753f1dc6ad
aaditya-1219 <aadityapatil1910@gmail.com>
2023-06-28 13:09:26 +0530

    Proper flow without tabs

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-function minToSec(min){
-    return min*60
-}
+// Periods
+// work
+// shortBreak
+// longBreak
 
-let work = true
-let sessionTime = minToSec(25)
-let breakTime = minToSec(5)
+function minToSec(min) {
+    return min * 60
+}
+let sessionTime = minToSec(0.05)
+let breakTime = minToSec(0.05)
 let currentSessionTime = sessionTime
 let currentBreakTime = breakTime
 let isRunning = false
+const clickSound = new Audio('../sound/click.mp3')
 let timer
 
 const timerTitle = document.getElementById('timer-title')
@@ -16,98 +20,129 @@ const pauseBtn = document.getElementById('pause')
 const container = document.getElementById('container')
 const period = document.getElementById('period')
 
-function handleClick(){
+const workTab = document.getElementById('workTab')
+const shortBreakTab = document.getElementById('shortBreakTab')
+let currentScreen = "work"
+workTab.classList.add('chosenScreen')
+
+
+function handleClick() {
     isRunning = !isRunning
-    if(isRunning){  // run the timer
+    if (isRunning) {  // run the timer
         pauseBtn.innerHTML = "PAUSE"
         pressButton()
-        if(work){startWork()}
-        else {startBreak()}
+        // if (work) { startWork() }
+        // else { startBreak() }
+        switch(currentScreen){
+            case "work":
+                startWork()
+                break
+            case "shortBreak":
+                startBreak()
+                break
+        }
+
     }
-    else{   // pause the timer
-        clearInterval(timer)
-        unpressButton()
+    else {   // pause the timer
         pauseBtn.innerHTML = "START"
+        unpressButton()
+        clearInterval(timer)
     }
 }
 
-function startWork(){
+function startWork() {
     timer = setInterval(() => {
-        if(currentSessionTime==0) {
-            work = false
-            restScreen()
-            // call resetTimer() after rest finish
+        if (currentSessionTime == 0) {
+            isRunning = false
+            unpressButton()
+            currentScreen = "shortBreak"
+            breakScreen()
+            clearInterval(timer)
         }
-        else{
+        else {
             currentSessionTime--
             timerTitle.innerHTML = formatTime(currentSessionTime)
         }
     }, 1000);
 }
 
-function restScreen(){
-    redToGreen()
-    unpressButton()
+function breakScreen() {
+    redToPurple()
     period.innerHTML = "Time for a Break!"
     timerTitle.innerHTML = formatTime(currentBreakTime)
     pauseBtn.innerHTML = "START"
 }
 
-function startBreak(){
+function workScreen() {
+    purpleToRed()
+    period.innerHTML = "Get to Work!"
+    timerTitle.innerHTML = formatTime(currentSessionTime)
+    pauseBtn.innerHTML = "START"
+}
+
+function startBreak() {
     timer = setInterval(() => {
-        if(currentBreakTime==0) {
-            isRunning = false
-            clearInterval(timer)
-            resetTimer()
-            // call resetTimer() after rest finish
+        if (currentBreakTime == 0) {
+            currentScreen = "work"
+            workScreen()
+            resetTimer()            
         }
-        else{
+        else {
             currentBreakTime--
             timerTitle.innerHTML = formatTime(currentBreakTime)
         }
     }, 1000);
 }
 
-function resetTimer(){
-    greenToRed()
-    unpressButton()
+function resetTimer() {
+    if (isRunning) {
+        isRunning = false
+        unpressButton()
+        pauseBtn.innerHTML = "START"
+    }
     clearInterval(timer)
-    work = true
-    pauseBtn.innerHTML = "START"
-    period.innerHTML = "Get to Work!"
-    timerTitle.innerHTML = formatTime(sessionTime) 
-    currentSessionTime = sessionTime
-    currentBreakTime = breakTime
+    currentSessionTime = sessionTime;
+    currentBreakTime = breakTime;
+    switch(currentScreen){
+        case "work":
+            timerTitle.innerHTML = formatTime(currentSessionTime)
+            break
+        case "shortBreak":
+            timerTitle.innerHTML = formatTime(currentBreakTime)
+            break
+    }
 }
 
-function redToGreen(){
+function redToPurple() {
     container.classList.remove('bg-red')
-    container.classList.add('bg-green')
+    container.classList.add('bg-purple')
     pauseBtn.classList.remove('text-red')
-    pauseBtn.classList.add('text-green')
+    pauseBtn.classList.add('text-purple')
 }
 
-function greenToRed(){
-    container.classList.remove('bg-green')
+function purpleToRed() {
+    container.classList.remove('bg-purple')
     container.classList.add('bg-red')
-    pauseBtn.classList.remove('text-green')
+    pauseBtn.classList.remove('text-purple')
     pauseBtn.classList.add('text-red')
 }
 
-function formatTime(sec){
-    let minutes = Math.floor(sec/60)
-    let seconds = sec-(60*minutes)
-    if(minutes<10) {minutes = "0"+minutes}
-    if(seconds<10) {seconds = "0"+seconds}
+function formatTime(sec) {
+    let minutes = Math.floor(sec / 60)
+    let seconds = sec - (60 * minutes)
+    if (minutes < 10) { minutes = "0" + minutes }
+    if (seconds < 10) { seconds = "0" + seconds }
     return minutes + ":" + seconds
 }
 
-function unpressButton(){
+function unpressButton() {
+    clickSound.play()
     pauseBtn.classList.remove('btn-pressed')
     pauseBtn.classList.add('btn-not-pressed')
 }
 
-function pressButton(){
+function pressButton() {
+    clickSound.play()
     pauseBtn.classList.remove('btn-not-pressed')
     pauseBtn.classList.add('btn-pressed')
 }
